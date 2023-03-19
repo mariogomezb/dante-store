b477fd3ba39feac6a32375be43b4ca518550ed6e
mariogomezb <mariogomezb@gmail.com>
2023-03-19 11:58:21 -0300

    14vo: validaciones

--- src/pages/IniciaSesion.jsx
+++ src/pages/IniciaSesion.jsx
@@ -1,29 +1,40 @@
 import React from 'react'
 import Footer from '../components/Footer'
 import Menu from '../components/Menu'
+import swal from 'sweetalert';
+import { useState } from 'react';
 
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 const IniciaSesion = () => {
+
+const [usermail, setUserMail] = useState("")
+const [userpassword, setUserPassword] = useState("")
+
+const handleSubmit =(e)=>{
+  e.preventDefault()
+  if (!usermail.trim()|| !userpassword.trim() ) swal("Favor ingresar todos los datos","","info");
+}
+
   return (
     <div className="container">
       <Menu/>
       <div className="row justify-content-center mt-5 mb-5">
       <div className="col-4">
-      <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email</Form.Label>
-        <Form.Control type="email" placeholder="Ingrese su e-mail" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Contraseña</Form.Label>
-        <Form.Control type="password" placeholder="Ingrese contraseña" />
-      </Form.Group>
-      <Button variant="dark" type="submit">
-        Ingresar
-      </Button>
-    </Form>
+      <Form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label"> Email</label>
+          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingrese su e-mail" onChange={(e)=>setUserMail(e.target.value)}></input>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label"> Contraseña</label>
+          <input type="password" className="form-control" id="exampleInputPassword1" aria-describedby="emailHelp" placeholder="Ingrese su contraseña" onChange={(e)=>setUserPassword(e.target.value)}></input>
+        </div>
+        <Button variant="dark" type="submit">
+          Ingresar
+        </Button>
+      </Form>
     <div className="d-flex mt-3">
       <div>
         <h7> ¿Aún no estás registrado?,</h7>
--- src/pages/Registro.jsx
+++ src/pages/Registro.jsx
@@ -1,34 +1,41 @@
 import React from 'react'
 import Footer from '../components/Footer'
 import Menu from '../components/Menu'
+import { useState } from 'react';
 
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 
 const Registro = () => {
+
+  const [usernombre, setUserNombre] = useState("")
+  const [usermail, setUserMail] = useState("")
+  const [userpassword, setUserPassword] = useState("")
+
+  const handleSubmit =(e)=>{
+    e.preventDefault()
+    if (!usermail.trim()|| !usernombre.trim()|| !userpassword.trim() ) swal("Favor ingresar todos los datos","","info");
+  }
+
   return (
     <div className="container">
     <Menu/>
     <div className="row justify-content-center mt-5 mb-5">
       <div className="col-4">
-      <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Nombre</Form.Label>
-        <Form.Control type="email" placeholder="Ingrese su nombre" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email</Form.Label>
-        <Form.Control type="email" placeholder="Ingrese su e-mail" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Teléfono</Form.Label>
-        <Form.Control type="email" placeholder="Ingrese su teléfono" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Contraseña</Form.Label>
-        <Form.Control type="password" placeholder="Ingrese contraseña" />
-      </Form.Group>
+      <Form onSubmit={handleSubmit}>
+      <div className="mb-3">
+          <label htmlFor="exampleInputName1" className="form-label"> Nombre</label>
+          <input type="text" className="form-control" placeholder="Ingrese su Nombre" onChange={(e)=>setUserNombre(e.target.value)}></input>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label"> Email</label>
+          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingrese su e-mail" onChange={(e)=>setUserMail(e.target.value)}></input>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label"> Contraseña</label>
+          <input type="password" className="form-control" id="exampleInputPassword1" aria-describedby="emailHelp" placeholder="Ingrese su contraseña" onChange={(e)=>setUserPassword(e.target.value)}></input>
+        </div>
       <Button variant="dark" type="submit">
         Registrarse
       </Button>
